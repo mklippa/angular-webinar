ab28d6caa644dbae5c04a2fea6fc3b518a0914c7
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2022-01-25 14:06:40 +0200

    task3-4-comments

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -6,6 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OrderByPipe implements PipeTransform {
   transform(value: any[], props: string[], isAsk: boolean = false): any[] {
     value.sort((a, b) => {
+      // при переходе к новому свойству, массив будет пересортировываться
+      // надо чтобы в рамках одного значения по предыдущему свойству происходила сортировка по текущему свойству
       for (const prop of props) {
         if (a[prop] < b[prop]) return isAsk ? -1 : 1;
         if (a[prop] > b[prop]) return isAsk ? 1 : -1;
